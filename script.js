0cf70bb052f3e17694b005855d4f49af3a35636a
Adem Hoskin <ademjhoskin@gmail.com>
2023-05-10 23:15:04 -0400

    fixed grid issues

--- script.js
+++ script.js
@@ -3,52 +3,59 @@ areaBtn.textContent = 'Change Area';
 document.body.appendChild(areaBtn);
 
 areaBtn.addEventListener('click', (e) => {
-    let gridValue = parseInt(prompt('Enter a number between 1 and 100'));
-    while (gridValue < 1 || gridValue > 100) {
+    let gridValueUpdated = parseInt(prompt('Enter a number between 1 and 100'));
+    while (gridValueUpdated < 1 || gridValueUpdated > 100) {
         gridValue = parseInt(prompt('Enter a number between 1 and 100'));
     }
-    return gridValueUpdated;
+    gridRemover();
+    gridBuilder(gridValueUpdated);
 });
 
-//DEFAULT GRID
-
-
+// create container div
 const container = document.createElement('div');
 container.setAttribute('class', 'container');
 container.style.display = 'flex';
 container.style.flexFlow = 'row wrap';
 document.body.appendChild(container);
+container.style.width = '800px';
+container.style.height = '800px';
 
-const gridBuilder = (gridValue = 22) => {
+// function to reinstantiate grid
+const gridRemover = () => {
+    const grid = document.querySelector('.container');
+    while (grid.firstChild) {
+        grid.removeChild(grid.firstChild);
+    };
+};
+
+// default grid is 16x16
+const gridBuilder = (gridValue = 16) => {
     for(let r = 0; r < gridValue; r++){
         const row = document.createElement('div');
         row.setAttribute('class', 'row');
         row.style.display = 'flex';
-        row.style.flexFlow = 'row wrap';
         row.style.width = '800px';
-        row.style.height = '0px';
+        row.style.height = `${800/gridValue}px}`;
+        row.style.flexFlow = 'row wrap';
         document.querySelector('.container').appendChild(row);
         for (let i = 0; i < gridValue ; i++) {
             const square = document.createElement('div');
             square.setAttribute('class', 'square');
-            square.style.width = `calc(800px / {gridValue}**2)`;
-            square.style.height = `calc(800px / {gridValue}**2)`;
+            square.style.boxSizing = 'border-box';
+            square.style.width = `${800/gridValue}px`;
+            square.style.height = `${800/gridValue}px`;
             square.style.border = '1px solid black';
-            square.style.padding = '1em';
             square.style.backgroundColor = 'white';
-            document.querySelector('.container').appendChild(square);
+            row.appendChild(square);
         };
     };
-};
-gridBuilder();
-
-const squares = document.querySelectorAll('.square');
-squares.forEach((square) => {
-    square.addEventListener('mouseover', (e) => {
-        e.target.style.backgroundColor = 'black';
-    }
+    const squares = document.querySelectorAll('.square');
+    squares.forEach((square) => {
+        square.addEventListener('mouseover', (e) => {
+            e.target.style.backgroundColor = 'black';
+        }
     );
 });
 
-
-
+};
+gridBuilder();
